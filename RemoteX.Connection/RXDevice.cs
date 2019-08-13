28bdad6cc83fba0552e32cf7cff2c0e3f1310eb4
陈俊嘉 <cjj19970505@live.cn>
2019-08-13 23:27:03 +0800

    先这样把

---
+++ RemoteX.Connection/RXDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteX.Connection
+{
+    public class RXDevice
+    {
+        public string DeviceName { get; internal set; }
+        public string DeviceId { get; internal set; }
+
+        internal RXDevice(string deviceName, string deviceId)
+        {
+            
+        }
+    }
+}
